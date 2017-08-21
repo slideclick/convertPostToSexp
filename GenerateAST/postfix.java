b9b578bdc8f7f3a4235b1219e9e39ca34e7813cc
slideclick <simonclick80@gmail.como>
2017-08-21 11:08:43 +0800

    for drjava move PostfixApp to first paragraph in code

--- GenerateAST/postfix.java
+++ GenerateAST/postfix.java
@@ -3,6 +3,36 @@
 // to run this program: C>java PostfixApp
 import java.io.*;              // for I/O
 ////////////////////////////////////////////////////////////////
+class PostfixApp
+   {
+   public static void main(String[] args) throws IOException
+      {
+      String input;
+      int output;
+
+      while(true)
+         {
+         System.out.print("Enter postfix: ");
+         System.out.flush();
+         input = getString();         // read a string from kbd
+         if( input.equals("") )       // quit if [Enter]
+            break;
+                                      // make a parser
+         ParsePost aParser = new ParsePost(input);
+         output = aParser.doParse();  // do the evaluation
+         System.out.println("Evaluates to " + output);
+         }  // end while
+      }  // end main()
+//--------------------------------------------------------------
+   public static String getString() throws IOException
+      {
+      InputStreamReader isr = new InputStreamReader(System.in);
+      BufferedReader br = new BufferedReader(isr);
+      String s = br.readLine();
+      return s;
+      }
+//--------------------------------------------------------------
+   }  // end class PostfixApp
 class StackX
    {
    private int maxSize;
@@ -101,34 +131,5 @@ class ParsePost
       }  // end doParse()
    }  // end class ParsePost
 ////////////////////////////////////////////////////////////////
-class PostfixApp
-   {
-   public static void main(String[] args) throws IOException
-      {
-      String input;
-      int output;
 
-      while(true)
-         {
-         System.out.print("Enter postfix: ");
-         System.out.flush();
-         input = getString();         // read a string from kbd
-         if( input.equals("") )       // quit if [Enter]
-            break;
-                                      // make a parser
-         ParsePost aParser = new ParsePost(input);
-         output = aParser.doParse();  // do the evaluation
-         System.out.println("Evaluates to " + output);
-         }  // end while
-      }  // end main()
-//--------------------------------------------------------------
-   public static String getString() throws IOException
-      {
-      InputStreamReader isr = new InputStreamReader(System.in);
-      BufferedReader br = new BufferedReader(isr);
-      String s = br.readLine();
-      return s;
-      }
-//--------------------------------------------------------------
-   }  // end class PostfixApp
 ////////////////////////////////////////////////////////////////
